390b219db567d8c0f0fb458e162a58ebef76144e
Dpbm <dpbm136@gmail.com>
2024-09-04 13:49:48 -0300

    feat: v2

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 def menu():
-    OPTIONS = ('d', 'e', 's', 'q')
+    OPTIONS = ('d', 'e', 's', 'q', 'nu', 'na', 'la')
     invalid = True
     selected_option = ''
 
     print(f"""
     ======================================
     Pressione:
-        [d] para depositar
-        [e] para retirar o extrato
-        [s] para realizar um saque
+        [d]  para depositar
+        [e]  para retirar o extrato
+        [s]  para realizar um saque
+        [nu] criar novo usuário
+        [na] criar nova conta
+        [la] listar contas
         [q] para sair
     ======================================
     """)
@@ -27,66 +30,110 @@ def quit_system():
     print("Obrigado por utilizar nosso sistema!")    
     sys.exit(0)
 
+def update_history(history, text):
+    history.append(text)
 
-def deposit():
-    invalid = True
-    value = 0
+def deposit(value, amount, history, /):
+    if(value <= 0):
+       print("Valor informado deve ser positivo e diferente de zero!")
+       return amount
 
-    while invalid:
-        value = float(input("==> Valor do depósito: "))
-        invalid = value <= 0
+    update_history(history, f'+ R$ {value:.2f}')
+    return amount + value
 
-        if(invalid):
-            print("Valor informado deve ser positivo e diferente de zero!")
+def withdrawal(*, value, amount, max_amount, max_withdrawals_per_day, total_withdrawal_today, history):
+    exceded_total = value > amount
+    exceded_max_value = value > max_amount
+    exceded_withdrawals_per_day = total_withdrawal_today >= max_withdrawals_per_day
+    invalid_number = value <= 0
+    invalid = exceded_max_value or exceded_total or invalid_number or exceded_withdrawals_per_day
+    
+    if(exceded_total):
+        print("Você não tem dinheiro suficiente para realizar esse saque!")
+    if(exceded_max_value):
+        print(f"O valor informado ultrapassa o limite do saque (R$ {max_amount:.2f})")
+    if(invalid_number):
+        print("O valor informado deve ser maior do que zero!")
+    if(exceded_withdrawals_per_day):
+        print(f"Você ultrapassou o maximo de saques hoje (maximo {max_withdrawals_per_day} por dia)!")
 
-    return value
-        
+    if(invalid):
+       return (amount, total_withdrawal_today)
 
-def withdrawal(account_total_sum):
-    MAX_AMOUNT_PER_WITHDRAWAL = 500
-    invalid = True
-    value = 0
+    update_history(history, f'- R$ {value:.2f}')
+    return (amount-value, total_withdrawal_today+1) 
 
-    while invalid:
-        value = float(input("==> Valor do saque: "))
-
-        exceded_total = value > account_total_sum
-        exceded_max_value = value > MAX_AMOUNT_PER_WITHDRAWAL
-        invalid_number = value <= 0
-        invalid = exceded_max_value or exceded_total or invalid_number
-
-        if(exceded_total):
-            print("Você não tem dinheiro suficiente para realizar esse saque!")
-        elif(exceded_max_value):
-            print("O valor informado ultrapassa o limite do saque (R$ 500.00)")
-        elif(invalid_number):
-            print("O valor informado deve ser maior do que zero!")
-
-    return value
-
-def statement(transactions, money_sum):
+def statement(amount, /, *, history):
     print(f"""
     ======================================
     Extrato detalhado:
     """)
 
-    if(len(transactions) <= 0):
+    if(len(history) <= 0):
         print("    Nenhuma transação foi realizada!")
     else:
-        for transaction in transactions:
+        for transaction in history:
             print(f"\t{transaction}")
     
     print(f"""
-    Total na conta: {money_sum:.2f}
+    Total na conta: {amount:.2f}
     ======================================
     """)
 
+def get_user(users, cpf):
+    users_found = [user for user in users if user['cpf'] == cpf]
+    return users_found[0] if len(users_found)>0 else None
+
+def create_user(users):
+    cpf = input("==> CPF: ")
+
+    if(get_user(users, cpf) is not None):
+        print("Usuário já cadastrado!")
+        return
+
+    name = input("==> Nome: ")
+    birth = input("==> Data de nascimento (dd-mm-aaaa): ")
+    address = input("==> Endereço (logradouro, nro - bairro - cidade/sigla estado): ")
+    users.append({'cpf':cpf, 'name':name, 'birth':birth, 'address':address})
+
+    print("Usuário criado com sucesso")
+
+def create_account(agency, account_number, users, accounts):
+    cpf = input("==> Informe o CPF: ")
+
+    user = get_user(users, cpf)
+    if(user is None):
+        print("Usuário não existe!")
+        return
+
+    accounts.append({"agency":agency, "account_number":account_number, 'cpf':cpf})
+
+def list_accounts(users, accounts):
+    
+    if(len(accounts) <= 0):
+        print("Nenhuma conta foi adicionada!")
+        return
+
+    print("\nContas:")
+    for account in accounts:
+        print('*'*50)
+        print(f"Agência: {account['agency']}")
+        print(f"Número da conta: {account['account_number']}")
+        user = get_user(users, account['cpf'])
+        print(f"Titular: {user['name']}")
+        print('*'*50)
 
 if __name__ == '__main__':
     MAX_WITHDRAWAL_PER_DAY = 3
+    MAX_WITHDRAWAL_VALUE = 500
     total_withdrawal_today = 0
+    
+    AGENCY = '0001'
 
-    money_sum = 0
+    users = []
+    accounts = []
+
+    amount = 0
     transactions = []
 
     try:
@@ -97,25 +144,35 @@ if __name__ == '__main__':
                 quit_system()
 
             elif(selected_option == 'd'):
-                value = deposit()
-                transactions.append(f'+ R$ {value:.2f}')
-                money_sum += value
+                value = float(input("==> Valor do depósito: ")) 
+                amount = deposit(value, amount, transactions)
 
             elif(selected_option == 's'):
-
-                if(total_withdrawal_today < MAX_WITHDRAWAL_PER_DAY):
-                    value = withdrawal(money_sum)
-                    transactions.append(f'- R$ {value:.2f}')
-                    money_sum -= value
-                    total_withdrawal_today += 1
-                else:
-                    print("você atingiu o limite máximo de saques hoje. Por valor, volte amanhã!")
+                value = float(input("==> Valor do saque: "))        
+                amount, total_withdrawal_today = withdrawal(
+                    value=value,
+                    amount=amount,
+                    max_amount=MAX_WITHDRAWAL_VALUE,
+                    max_withdrawals_per_day=MAX_WITHDRAWAL_PER_DAY,
+                    total_withdrawal_today=total_withdrawal_today,
+                    history=transactions,
+                )
             
+            elif(selected_option == 'nu'):
+                create_user(users)
+
+            elif(selected_option == 'na'):
+                account_number = len(accounts)+1
+                create_account(AGENCY, account_number, users, accounts)
+
+            elif(selected_option == 'la'):
+                list_accounts(users, accounts)
+
             else:
-                statement(transactions, money_sum)
+                statement(amount, history=transactions)
 
     except KeyboardInterrupt:
         quit_system()
     except Exception as error:
-        print(f"Ocorreu um error: {str(error)}")
+        print(f"Ocorreu um erro: {str(error)}")
         quit_system()
